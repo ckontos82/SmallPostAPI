54ec5b4e8c37a795eac2079aa7bfe3c7618bf04d
harko <harko@DAEMONSLAPTOP>
2025-10-19 02:31:59 +0300

    Added Healthchecks and exposing Assembly Version and status.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,49 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using SmallPostAPI.Data;
 using SmallPostAPI.Services;
 using SmallPostAPI.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Scalar.AspNetCore;
+using System.Reflection;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
 builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
 
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var payload = new
+        {
+            status = report.Status.ToString(),
+            version = Assembly.GetExecutingAssembly().GetName().Version
+        };
+        await context.Response.WriteAsync(JsonSerializer.Serialize(payload));
+    }
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapControllers();
 
