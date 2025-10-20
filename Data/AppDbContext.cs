11a7ba4b9d2489a48cf950678ef243f206c8b5c1
harko <harko@DAEMONSLAPTOP>
2025-10-21 01:12:31 +0300

    Removed Friendship from master branch

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,7 +7,6 @@ namespace SmallPostAPI.Data
     {
         public DbSet<User> Users => Set<User>();
         public DbSet<Post> Posts => Set<Post>();
-        public DbSet<Friendship> Friendships => Set<Friendship>();
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
@@ -41,37 +40,6 @@ namespace SmallPostAPI.Data
                     .WithMany(u => u.Posts)
                     .HasForeignKey(p => p.UserId)
                     .OnDelete(DeleteBehavior.Cascade); 
-            });
-
-            modelBuilder.Entity<Friendship>(b =>
-            {
-                b.ToTable("Friendships", t =>
-                {
-                    t.HasCheckConstraint(
-                        "CK_Friendships_UnorderedPair",
-                        "[UserId] < [FriendId]"
-                    );
-                });
-
-                b.HasKey(x => new { x.UserId, x.FriendId });
-
-                b.HasOne(x => x.User)
-                 .WithMany()
-                 .HasForeignKey(x => x.UserId)
-                 .OnDelete(DeleteBehavior.Cascade);
-
-                b.HasOne(x => x.Friend)
-                 .WithMany()
-                 .HasForeignKey(x => x.FriendId)
-                 .OnDelete(DeleteBehavior.Cascade);
-
-                b.HasOne(x => x.RequestedByUser)
-                 .WithMany()
-                 .HasForeignKey(x => x.RequestedByUserId)
-                 .OnDelete(DeleteBehavior.Restrict);
-
-                b.HasIndex(x => new { x.UserId, x.Status });
-                b.HasIndex(x => new { x.FriendId, x.Status });
             });
         }
     }
