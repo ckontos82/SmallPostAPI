a532ac92b0218cd05d955f675e9fff0863fd0c52
harko <harko@DAEMONSLAPTOP>
2025-10-20 22:25:44 +0300

    Added Friendship model, Status enum, and Frienship DBSet in DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace SmallPostAPI.Data
 
         public DbSet<User> Users => Set<User>();
         public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Friendship> Friendships => Set<Friendship>();
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
@@ -44,6 +46,37 @@ namespace SmallPostAPI.Data
                     .WithMany(u => u.Posts)
                     .HasForeignKey(p => p.UserId)
                     .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            modelBuilder.Entity<Friendship>(b =>
+            {
+                b.ToTable("Friendships", t =>
+                {
+                    t.HasCheckConstraint(
+                        "CK_Friendships_UnorderedPair",
+                        "[UserId] < [FriendId]"
+                    );
+                });
+
+                b.HasKey(x => new { x.UserId, x.FriendId });
+
+                b.HasOne(x => x.User)
+                 .WithMany()
+                 .HasForeignKey(x => x.UserId)
+                 .OnDelete(DeleteBehavior.Cascade);
+
+                b.HasOne(x => x.Friend)
+                 .WithMany()
+                 .HasForeignKey(x => x.FriendId)
+                 .OnDelete(DeleteBehavior.Cascade);
+
+                b.HasOne(x => x.RequestedByUser)
+                 .WithMany()
+                 .HasForeignKey(x => x.RequestedByUserId)
+                 .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasIndex(x => new { x.UserId, x.Status });
+                b.HasIndex(x => new { x.FriendId, x.Status });
             });
         }
     }
