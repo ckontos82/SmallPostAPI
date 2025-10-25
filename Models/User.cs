b2a71ed4863dee2a5ed4db86cafc035d2463585f
harko <harko@DAEMONSLAPTOP>
2025-10-25 03:29:10 +0300

    Adding Friendship Model

--- Models/User.cs
+++ Models/User.cs
@@ -6,5 +6,8 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public ICollection<Post> Posts { get; set; } = new List<Post>();
+
+        public ICollection<Friendship> FriendshipsInitiated { get; } = new List<Friendship>();
+        public ICollection<Friendship> FriendshipsReceived { get; } = new List<Friendship>();
     }
 }
